e8e7bf1f94103686fcd31bd2d9e080a808a22aaf
harshu-1310 <harshithsai06@gmail.com>
2026-05-02 13:33:30 +0530

    Fix case-sensitive imports for Render

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/task");
+const Task = require("../models/Task");
 const auth = require("../middleware/auth");
 
 // CREATE TASK (ADMIN)
