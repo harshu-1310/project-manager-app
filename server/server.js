864b249250dd8ef0abaf1ada984c216318212a69
harshu-1310 <harshithsai06@gmail.com>
2026-05-02 13:25:11 +0530

    Render deployment setup

--- server/server.js
+++ server/server.js
@@ -15,4 +15,6 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
